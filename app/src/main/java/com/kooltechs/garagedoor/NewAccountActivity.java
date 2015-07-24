4749f3ac1ac39f7ad7ff173c783b57bef04144b9
arthurcao <ngoctienbkit@yahoo.com>
2015-07-24 11:16:39 +0700

    create activity

--- app/src/main/java/com/kooltechs/garagedoor/NewAccountActivity.java
+++ app/src/main/java/com/kooltechs/garagedoor/NewAccountActivity.java
@@ -1,40 +1,23 @@
 package com.kooltechs.garagedoor;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.kooltechs.garagedoor.dns_api.DNSService;
 
-
-public class MainActivity extends ActionBarActivity {
+public class NewAccountActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //https://github.com/arthurcao/GarageDoor.git
-        //https://code.google.com/p/mdnsjava/source/browse/tags/mdnsjava_2.1.5/src/main/java/Tester.java
-        //https://code.google.com/p/mdnsjava/
-        //https://justanapplication.wordpress.com/2013/11/03/service-discovery-in-android-and-ios-part-three-if-you-want-something-done-properly-mdns-and-dns-service-discovery-basics/
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-//        new Thread(){
-//            @Override
-//            public void run() {
-//                DNSService.test();
-//
-//            }
-//        }.start();
+        setContentView(R.layout.activity_new_account);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_account, menu);
         return true;
     }
 
--- app/src/main/java/com/kooltechs/garagedoor/dns_api/DNSService.java
+++ app/src/main/java/com/kooltechs/garagedoor/dns_api/DNSService.java
@@ -1,20 +1,33 @@
 package com.kooltechs.garagedoor.dns_api;
 
 import android.util.Log;
-
-import java.net.InetAddress;
-
 import org.xbill.DNS.Name;
 import org.xbill.DNS.Record;
+import org.xbill.DNS.TextParseException;
 import org.xbill.DNS.Type;
 import org.xbill.mDNS.Lookup;
 import org.xbill.mDNS.MulticastDNSService;
 import org.xbill.mDNS.ServiceInstance;
 import org.xbill.mDNS.ServiceName;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class DNSService {
 
-    public static void test(){
+    private MulticastDNSService mMulticastDNSService;
+    private ServiceInstance mServiceInstance;
+
+    private DNSService INSTANCE = new DNSService();
+    private DNSService(){
+    }
+
+    public DNSService getInstance(){
+        return INSTANCE;
+    }
+
+    public void register(){
+        unregister();
         int priority = 10;
         int weight = 10;
         int port = 8080;
@@ -23,20 +36,17 @@ public class DNSService {
 //        String ucnPTRName = "_mdc._tcp";
         String domain = "local.";
         String hostname = "TestHost";
-
-        try
-        {
-            MulticastDNSService service = new MulticastDNSService();
-
+        try {
             Name fqn = new Name(hostname + "." + (domain.endsWith(".") ? domain : domain + "."));
             ServiceName srvName = new ServiceName(hostname + "." + ucnPTRName + "." + (domain.endsWith(".") ? domain : domain + "."));
 
+            mMulticastDNSService = new MulticastDNSService();
             ServiceInstance serviceInstance = new ServiceInstance(srvName, priority, weight, port, fqn, new InetAddress[] {InetAddress.getByName("192.168.1.74")}, "textvers=1", "rn=urn:smpte:udn:local:id=1234567890ABCDEF", "proto=mdcp", "path=/Device");
-            ServiceInstance registeredService = service.register(serviceInstance);
-            if (registeredService != null){
-                Log.e("test", "Services Successfully Registered: \n\t" + registeredService);
-            } else
-            {
+            mServiceInstance = mMulticastDNSService.register(serviceInstance);
+            if (mServiceInstance != null){
+                Log.e("test", "Services Successfully Registered:" + mServiceInstance);
+            }else{
+                Log.e("test","Services fail Registered");
                 boolean hostnameResolves = false;
 
                 Lookup lookup = new Lookup(fqn);
@@ -64,39 +74,113 @@ public class DNSService {
                     }
                 }
 
-                Log.e("test","Services Registration for UCN \"" + ucn + "\" in domain \"" + domain + "\" Failed!");
-                if (hostnameResolves)
-                {
-                    Log.e("test","Hostname \"" + fqn + "\" can be resolved.");
-                } else
-                {
-                    Log.e("test","Hostname \"" + fqn + "\" cannot be resolved!");
+                Log.e("test", "Services Registration for UCN \"" + ucn + "\" in domain \"" + domain + "\" Failed!");
+                if (hostnameResolves){
+                    Log.e("test", "Hostname \"" + fqn + "\" can be resolved.");
+                } else{
+                    Log.e("test", "Hostname \"" + fqn + "\" cannot be resolved!");
                 }
             }
 
-            while (true)
-            {
-                Thread.sleep(10);
-                if (System.in.read() == 'q')
+
+        } catch (TextParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void unregister(){
+        if(mMulticastDNSService != null && mServiceInstance != null){
+            try {
+                if (mMulticastDNSService.unregister(mServiceInstance))  {
+                    System.out.println("Services Successfully Unregistered: \n\t" + mServiceInstance);
+                } else  {
+                    System.err.println("Services was not Unregistered: \n\t" + mServiceInstance);
+                }
+                mMulticastDNSService.close();
+                mMulticastDNSService = null;
+                mServiceInstance = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void test(){
+        int priority = 10;
+        int weight = 10;
+        int port = 8080;
+        String ucn = "urn:smpte:ucn:org.smpte.st2071:device_v1.0";
+        String ucnPTRName = "_org.smpte.st2071:device_v1.0._sub._mdc._tcp";
+//        String ucnPTRName = "_mdc._tcp";
+        String domain = "local.";
+        String hostname = "TestHost";
+        try {
+            Name fqn = new Name(hostname + "." + (domain.endsWith(".") ? domain : domain + "."));
+            ServiceName srvName = new ServiceName(hostname + "." + ucnPTRName + "." + (domain.endsWith(".") ? domain : domain + "."));
+            MulticastDNSService service = new MulticastDNSService();
+            ServiceInstance serviceInstance = new ServiceInstance(srvName, priority, weight, port, fqn, new InetAddress[] {InetAddress.getByName("192.168.1.74")}, "textvers=1", "rn=urn:smpte:udn:local:id=1234567890ABCDEF", "proto=mdcp", "path=/Device");
+            ServiceInstance registeredService = service.register(serviceInstance);
+            if (registeredService != null){
+                Log.e("test","Services Successfully Registered:" + registeredService);
+            }else{
+                Log.e("test","Services fail Registered");
+                boolean hostnameResolves = false;
+
+                Lookup lookup = new Lookup(fqn);
+                Record[] rrs = lookup.lookupRecords();
+                if (rrs != null && rrs.length > 0)
                 {
+                    outer:
+                    for (Record rr : rrs)
+                    {
+                        switch (rr.getType())
+                        {
+                            case Type.A:
+                            case Type.A6:
+                            case Type.AAAA:
+                            case Type.DNAME:
+                            case Type.CNAME:
+                            case Type.PTR:
+                                if (rr.getName().equals(fqn))
+                                {
+                                    hostnameResolves = true;
+                                    break outer;
+                                }
+                                break;
+                        }
+                    }
+                }
+
+                Log.e("test", "Services Registration for UCN \"" + ucn + "\" in domain \"" + domain + "\" Failed!");
+                if (hostnameResolves){
+                    Log.e("test", "Hostname \"" + fqn + "\" can be resolved.");
+                } else{
+                    Log.e("test", "Hostname \"" + fqn + "\" cannot be resolved!");
+                }
+            }
+
+            while (true)  {
+                Thread.sleep(10);
+                if (System.in.read() == 'q') {
                     break;
                 }
             }
 
-            if (service.unregister(registeredService))
-            {
-                Log.e("test", "Services Successfully Unregistered: \n\t" + registeredService);
-            } else
-            {
-                Log.e("test","Services was not Unregistered: \n\t" + registeredService);
+            if (service.unregister(registeredService))  {
+                System.out.println("Services Successfully Unregistered: \n\t" + registeredService);
+            } else  {
+                System.err.println("Services was not Unregistered: \n\t" + registeredService);
             }
 
             service.close();
-            System.exit(0);
-        } catch (Exception e)
-        {
-            Log.e("test","Error Registering Capability \"" + ucn + "\" for Discovery - " + e.getMessage());
-            e.printStackTrace(System.err);
+        } catch (TextParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
